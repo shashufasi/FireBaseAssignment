3da335a3fdf3f6da0ceb83b565b2d5c2a5a5d9b3
Faisal Qazi <faisalqazi31@gmail.com>
2018-12-09 14:15:46 +0500

    commit and push

--- app/src/main/java/com/example/shashu/firebaseassignment/MainActivity.java
+++ app/src/main/java/com/example/shashu/firebaseassignment/MainActivity.java
@@ -1,39 +1,81 @@
 package com.example.shashu.firebaseassignment;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Context context;
+    RecyclerView recyclerView;
     EditText txtName;
     Button btnAddArtist;
     Spinner spinGenre;
     DatabaseReference databaseArtists;
+    List<Artist> artistList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = MainActivity.this;
         databaseArtists = FirebaseDatabase.getInstance().getReference("artists");
+
         //Finding Controls By Ids and Referencing them...
         txtName = findViewById(R.id.txtName);
         btnAddArtist = findViewById(R.id.btnAddArtist);
         spinGenre = findViewById(R.id.spinGenre);
-
+        recyclerView = findViewById(R.id.recyclerList);
+        artistList = new ArrayList<>();
         //Adding Click Listener to Button Add Artist...
         btnAddArtist.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(txtName.getWindowToken(), 0);
                 addArtist();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        databaseArtists.addValueEventListener(new ValueEventListener() {
+
+            //OnDataChange method will be Executed Every Time when data has been changed...
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                artistList.clear();
+                for (DataSnapshot artistSnapShot : dataSnapshot.getChildren()) {
+                    Artist artist = artistSnapShot.getValue(Artist.class);
+                    artistList.add(artist);
+                }
+                recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                recyclerView.setAdapter(new ArtistAdapter(MainActivity.this, artistList));
+            }
+
+            //OnCancelled will be executed everytime an ERROR occurs...
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
@@ -51,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
             Artist artist = new Artist(id, name, genre);
             databaseArtists.child(id).setValue(artist);
             Toast.makeText(this, "Artist Added", Toast.LENGTH_LONG).show();
-
+            txtName.setText("");
         } else {
             Toast.makeText(this, "You should Enter Name", Toast.LENGTH_LONG).show();
         }
